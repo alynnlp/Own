a692e90e7f13876c87ea351fb5e9912b1c81c6a3
Aileen Pang <aileenpang63@gmail.com>
2017-11-17 17:14:56 -0500

    client can edit their own profile

--- app/controllers/client_profile_controller.rb
+++ app/controllers/client_profile_controller.rb
@@ -16,45 +16,35 @@ class ClientProfileController < ApplicationController
 
   def new
         @register = Register.new
-        respond_to do |format|
-          format.html
-          format.json { render json: @register}
-        end
-      end
+  end
 
   def create
-    @user = User.new(register_params)
 
-    respond_to do |format|
-      if @register.save
-        format.html { redirect_to @register, notice: "Save process completed!" }
-        format.json { render json: @register, status: :created, location: @register }
-      else
-        format.html {
-          flash.now[:notice]="Save proccess coudn't be completed!"
-          render :new
-        }
-        format.json { render json: @register.errors, status: :unprocessable_entity}
-      end
-    end
   end
 
   def edit
-    @agent = Agent.find(params[:id])
+    @user = User.find(params[:id])
   end
+
   def update
-    @agent = Agent.find(params[:id])
-    if @agent.update_attributes(agent_params)
-      redirect_to edit_agent_profile_path, :notice => "Your profile has been updated"
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      redirect_to client_profile_path, :notice => "Your profile has been updated"
     else
       render "show"
     end
   end
+
   def destroy
   end
 
   private
     def register_params
       params.permit(:name, :email, :password)
+    end
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,:location, :budget, :size, :age, :address, :occupation)
     end
 end
